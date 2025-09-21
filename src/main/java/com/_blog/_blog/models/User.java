2b3d447e77dd7c271d76538cc391757b4d7bffd3
zakariasalhi12 <rafayelle2005@gmail.com>
2025-09-21 06:49:02 +0100

    add server port in config file

---
+++ src/main/java/com/_blog/_blog/models/User.java
@@ -0,0 +1,10 @@
+package com._blog._blog.models;
+
+public class User {
+    public String username;
+    public String password;
+    public String email;
+    public String image;
+    public String gender;
+    public int age;
+}
