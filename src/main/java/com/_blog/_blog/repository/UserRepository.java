1cdea1fe81f31b6e3fca403f7fd8212f223c237e
zakariasalhi12 <rafayelle2005@gmail.com>
2025-09-24 18:33:28 +0100

    add frontend folder

--- src/main/java/com/_blog/_blog/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com._blog._blog.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com._blog._blog.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // User findByUsername(String username); 
-}
